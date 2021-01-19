1ccf13c103b5fcf89ff391dbbbbcdcf6f4b3fded
Olga Mostovetsky <asila527@gmail.com>
2021-01-18 17:19:34 -0800

    product categories rendered

--- src/components/CategoriesList.react.js
+++ src/components/CategoriesList.react.js
@@ -1,43 +1,42 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux'
 import { fetchProducts } from '../actions/productsActions'
+import { useSelector, useDispatch } from 'react-redux'
+
 
 function CategoriesList ({ productData, fetchProducts }) {
     useEffect(() => {
         fetchProducts()
     }, [])
-    
-    return <h1>Test</h1>
-    // return productData.loading ? (
-    //     <h2>Loading Text</h2>
-    // ) : productData.error ? (
-    //     <h2>{productData.error}</h2>
-    // ) : (
-    //     <div>
-    //         <h2>Product List</h2>
-    //         <div>
-    //             {productData && 
-    //                 productData.products && 
-    //                 productData.products.map(product => <p key={product.id}>{product.category}</p>)
-    //             }
-    //         </div>
-    //     </div>
-    // )
+    // const products = useSelector(state => state.products)
+    // const dispatch = useDispatch()    
+    // return <h1>Test</h1>
+    return productData.loading ? (
+        <h2>Loading Text</h2>
+    ) : productData.error ? (
+        <h2>{productData.error}</h2>
+    ) : (
+        <div>
+            <h2>Product List</h2>
+            <div>
+                {productData && 
+                    productData.products && 
+                    productData.products.map(product => <p key={product.id}>{product.category}</p>)
+                }
+            </div>
+        </div>
+    )
 }
-
 
 const mapStateToProps = state => {
     return {
         productData: state.product
     }
 }
-
-
-
 const mapDispatchToProps = dispatch => {
     return {
         fetchProducts: () => dispatch(fetchProducts())
     }
 }
-
 export default connect(mapStateToProps, mapDispatchToProps)(CategoriesList)
+// export default CategoriesList
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import productsReducer from './productsReducer';
 
 const rootReducer = combineReducers({
-    data: productsReducer
+    product: productsReducer
 })
 
 export default rootReducer
